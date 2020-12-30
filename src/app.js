96ca39059d4ecdba1ab8212ea8a8857ae5cd46e2
jmejia <jmejiaxdev@gmail.com>
2020-12-30 14:17:35 -0400

    Add routes and views

--- src/app.js
+++ src/app.js
@@ -1,12 +1,23 @@
 const app = require("express")();
+const expressHandlebars = require("express-handlebars");
 const dotenv = require("dotenv");
+var path = require("path");
 const connectDb = require("./config/db");
 
-const PORT = 3000;
+const port = 3000;
 
 // Load dotenv config
 dotenv.config();
 
+// Mongoose
 connectDb();
 
-app.listen(PORT, console.log("Listening at port 3000"));
+// Handlebars
+app.engine(".hbs", expressHandlebars({ defaultLayout: "main", extname: ".hbs" })); // Express template engine
+app.set("view engine", ".hbs");
+app.set("views", path.join(__dirname, "./views/"));
+
+// Routes
+app.use("/", require("./routes/index.js"));
+
+app.listen(port, console.log("Listening at port 3000"));
